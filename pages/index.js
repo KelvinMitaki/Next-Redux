483b0f5f4b82665f536314bf1faf1a2920958217
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-24 16:24:14 +0300

    added semantic ui styling

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { fetchPosts } from "../redux/actions";
 import { connect } from "react-redux";
 import { wrapper } from "../redux/reducers";
-import { Container } from "semantic-ui-react";
+import { Container, List } from "semantic-ui-react";
 
 export class index extends Component {
   static async getInitialProps({ store }) {
@@ -10,14 +10,23 @@ export class index extends Component {
   }
   render() {
     return (
-      <Container textAlign="center">
-        {this.props.posts &&
-          this.props.posts.map(post => (
-            <div key={post.id}>
-              <h3>{post.title}</h3>
-              <p>{post.body}</p>
-            </div>
-          ))}
+      <Container>
+        <List divided relaxed>
+          {this.props.posts &&
+            this.props.posts.map(post => (
+              <List.Item key={post.id}>
+                <List.Icon name="github" size="large" verticalAlign="middle" />
+                <List.Content>
+                  <List.Header as="a">
+                    <h3>{post.title}</h3>
+                  </List.Header>
+                  <List.Description as="a">
+                    <p>{post.body}</p>
+                  </List.Description>
+                </List.Content>
+              </List.Item>
+            ))}
+        </List>
       </Container>
     );
   }
